25cb5f2ef6b29185ac90b993ada56a82fedcdd71
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-22 13:44:32 -0300

    introduction

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.component('dhom-component', {
+  data: function() {
+    return {
+      titulo: 'Diego'
+    }
+  },
+  template: '<h1>{{titulo}}</h1>s'
+})
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app'
+})
