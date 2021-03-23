ac780e2f53980cb5609e7627109a969341e2ced6
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-23 10:22:14 -0300

    data as a function

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 import Vue from 'vue'
 
+const data = {
+  titulo: 'Vue Component',
+  contador: 0
+}
+
 Vue.component('dhom-component', {
   data: function() {
-    return {
-      titulo: 'Diego'
+    return data
+  },
+  methods: {
+    incrementar() {
+      this.contador++;
     }
   },
-  template: '<h1>{{titulo}}</h1>s'
+  template:`
+    <div>
+      <h1>{{ titulo }}</h1>
+      <button @click="incrementar">Clicado {{ contador }}</button>
+    <div>
+  `
 })
+
 
 new Vue({
   el: '#app'
